cfb782840e97cc1f39d8e9a471acabc6d36fb69f
Philibert Latournerie <philibert.latournerie@hotmail.fr>
2017-02-21 16:44:18 +0100

    add comment to resto" "

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,17 +2,25 @@ class CommentsController < ApplicationController
 
 def create
   @project = Project.find(params[:project_id])
-  @comment = Comment.new(user: current_user, project: @project)
-  authorize @comment
-  @comment.save
-  redirect_to project_path(@project)
+  @comment = Comment.new(comment_params)
+  @comment.project = @project
+  if @comment.save
+    redirect_to project_path(@project)
+  else
+    render 'projects/show'
+  end
 end
+
+
 
 def destroy
   @comment.destroy
   redirect_to project_path(@project)
 end
 
-
+private
+def review_params
+    params.require(:comment).permit(:content)
+end
 
 end
