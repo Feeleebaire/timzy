7f953798e66c9129c616d9b9544bd27232572dd9
Cesar Lacaille <cesar.lacaille@hotmail.fr>
2017-02-21 14:22:19 +0100

    devise controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :projects
-  devise_for :users
+  devise_for :users, controllers: {
+        registrations: 'users/registrations'
+      }
   root to: 'pages#home'
 
   resources :teams, only: [:show, :create, :new] do
