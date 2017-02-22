cdefbb9925bc4452e4ba73e91258f8ffbe2b1a5b
Sarah Reneaume <sarah.reneaume@gmail.com>
2017-02-22 17:03:03 +0100

    first attempt

--- app/policies/project_policy.rb
+++ app/policies/project_policy.rb
@@ -5,8 +5,12 @@ class ProjectPolicy < ApplicationPolicy
     end
   end
 
+  def index
+    is_user_team_member?
+  end
+
   def show?
-    true
+   is_user_team_member?
   end
 
   def new?
@@ -29,6 +33,10 @@ class ProjectPolicy < ApplicationPolicy
   end
 
   private
+
+  def is_user_team_member?
+    record.team.users.include? user
+  end
 
   def is_user_owner_or_admin?
     record.user == user || record.team.admin == user
