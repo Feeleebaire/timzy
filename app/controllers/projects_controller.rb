5c0f64ed75b3680b23806296320d1b6f281274d2
Cesar Lacaille <cesar.lacaille@hotmail.fr>
2017-03-01 12:24:45 +0100

    y labels updated

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,6 +7,8 @@ class ProjectsController < ApplicationController
     @comment = Comment.new
     authorize(@project)
     uri = URI::parse(@project.url_targeted)
+    @startdate = params[:startdate].blank? ? "30daysAgo" : params[:startdate]
+    @enddate = params[:enddate].blank? ? "yesterday" : params[:enddate]
     @ga = GoogleApi::Analytics.new(@project.team.admin)
     @service = @ga.service
     # SET GOALS KPI
@@ -14,7 +16,7 @@ class ProjectsController < ApplicationController
     @kpi = params[:kpi].blank? ? @project.kpi : params[:kpi]
 
     if !@kpi.blank?
-      @datacustom = @service.get_ga_data("ga:#{@project.team.view_id}", "30daysAgo", "yesterday", "ga:goal#{@kpi}completions", dimensions: "ga:date")
+      @datacustom = @service.get_ga_data("ga:#{@project.team.view_id}", "#{@startdate}", "#{@enddate}", "ga:goal#{@kpi}completions", dimensions: "ga:date")
       @arraycustom = @datacustom.rows
       @graphcustom = []
       @arraycustom.each do |data|
@@ -23,14 +25,15 @@ class ProjectsController < ApplicationController
         hash[:y] = data[1]
         @graphcustom << hash
       end
+      @custom_dates = set_dates_for_graph(@arraycustom)
     end
 
     # SET EVENT KPI
-    @kpievent = @service.get_ga_data("ga:#{@project.team.view_id}", "360daysAgo", "yesterday", "ga:totalEvents", dimensions: "ga:eventAction")
+    @kpievent = @service.get_ga_data("ga:#{@project.team.view_id}", "#{@startdate}", "#{@enddate}", "ga:totalEvents", dimensions: "ga:eventAction")
 
     if !@kpievent.rows.blank?
       @event = params[:event].blank? ? @kpievent.rows.first.first : params[:event]
-      @datacustomevent = @service.get_ga_data("ga:#{@project.team.view_id}", "30daysAgo", "yesterday", "ga:totalEvents", dimensions: "ga:date", filters: "ga:eventAction==#{@event}")
+      @datacustomevent = @service.get_ga_data("ga:#{@project.team.view_id}", "#{@startdate}", "#{@enddate}", "ga:totalEvents", dimensions: "ga:date", filters: "ga:eventAction==#{@event}")
       @arraycustomevent = @datacustomevent.rows
       @graphcustomevent = []
       @arraycustomevent.each do |data|
@@ -39,21 +42,22 @@ class ProjectsController < ApplicationController
         hash[:y] = data[1]
         @graphcustomevent << hash
       end
+      @customevent_dates = set_dates_for_graph(@arraycustomevent)
     end
 
     # Basic Metrics
     if uri.path.blank?
-      @datasession = @service.get_ga_data("ga:#{@project.team.view_id}", "30daysAgo", "yesterday", "ga:sessions", dimensions: "ga:date")
-      @datasuser = @service.get_ga_data("ga:#{@project.team.view_id}", "30daysAgo", "yesterday", "ga:users", dimensions: "ga:date")
-      @datapv = @service.get_ga_data("ga:#{@project.team.view_id}", "30daysAgo", "yesterday", "ga:pageviews", dimensions: "ga:date")
-      @databr = @service.get_ga_data("ga:#{@project.team.view_id}", "30daysAgo", "yesterday", "ga:bounceRate")
-      @datanv = @service.get_ga_data("ga:#{@project.team.view_id}", "30daysAgo", "yesterday", "ga:percentNewSessions")
+      @datasession = @service.get_ga_data("ga:#{@project.team.view_id}", "#{@startdate}", "#{@enddate}", "ga:sessions", dimensions: "ga:date")
+      @datasuser = @service.get_ga_data("ga:#{@project.team.view_id}", "#{@startdate}", "#{@enddate}", "ga:users", dimensions: "ga:date")
+      @datapv = @service.get_ga_data("ga:#{@project.team.view_id}", "#{@startdate}", "#{@enddate}", "ga:pageviews", dimensions: "ga:date")
+      @databr = @service.get_ga_data("ga:#{@project.team.view_id}", "#{@startdate}", "#{@enddate}", "ga:bounceRate")
+      @datanv = @service.get_ga_data("ga:#{@project.team.view_id}", "#{@startdate}", "#{@enddate}", "ga:percentNewSessions")
     else
-      @datasession = @service.get_ga_data("ga:#{@project.team.view_id}", "30daysAgo", "yesterday", "ga:sessions", dimensions: "ga:date", filters: "ga:pagePath=@#{uri.path}")
-      @datasuser = @service.get_ga_data("ga:#{@project.team.view_id}", "30daysAgo", "yesterday", "ga:users", dimensions: "ga:date", filters: "ga:pagePath=@#{uri.path}")
-      @datapv = @service.get_ga_data("ga:#{@project.team.view_id}", "30daysAgo", "yesterday", "ga:pageviews", dimensions: "ga:date", filters: "ga:pagePath=@#{uri.path}")
-      @databr = @service.get_ga_data("ga:#{@project.team.view_id}", "30daysAgo", "yesterday", "ga:bounceRate", filters: "ga:pagePath=@#{uri.path}")
-      @datanv = @service.get_ga_data("ga:#{@project.team.view_id}", "30daysAgo", "yesterday", "ga:percentNewSessions", filters: "ga:pagePath=@#{uri.path}")
+      @datasession = @service.get_ga_data("ga:#{@project.team.view_id}", "#{@startdate}", "#{@enddate}", "ga:sessions", dimensions: "ga:date", filters: "ga:pagePath=@#{uri.path}")
+      @datasuser = @service.get_ga_data("ga:#{@project.team.view_id}", "#{@startdate}", "#{@enddate}", "ga:users", dimensions: "ga:date", filters: "ga:pagePath=@#{uri.path}")
+      @datapv = @service.get_ga_data("ga:#{@project.team.view_id}", "#{@startdate}", "#{@enddate}", "ga:pageviews", dimensions: "ga:date", filters: "ga:pagePath=@#{uri.path}")
+      @databr = @service.get_ga_data("ga:#{@project.team.view_id}", "#{@startdate}", "#{@enddate}", "ga:bounceRate", filters: "ga:pagePath=@#{uri.path}")
+      @datanv = @service.get_ga_data("ga:#{@project.team.view_id}", "#{@startdate}", "#{@enddate}", "ga:percentNewSessions", filters: "ga:pagePath=@#{uri.path}")
     end
 
     @arraysession = @datasession.rows
@@ -64,6 +68,7 @@ class ProjectsController < ApplicationController
       hash[:y] = data[1]
       @graphsession << hash
     end
+    @session_dates = set_dates_for_graph(@arraysession)
 
     @arrayuser = @datasuser.rows
     @graphuser = []
@@ -73,6 +78,7 @@ class ProjectsController < ApplicationController
       hash[:y] = data[1]
       @graphuser << hash
     end
+    @user_dates = set_dates_for_graph(@arrayuser)
 
     @arraypv = @datapv.rows
     @graphpv = []
@@ -82,6 +88,7 @@ class ProjectsController < ApplicationController
       hash[:y] = data[1]
     @graphpv << hash
     end
+    @pv_dates = set_dates_for_graph(@arraypv)
 
     @data_br = [ @databr.rows.first.first.to_f, 100 - @databr.rows.first.first.to_f ]
 
@@ -139,9 +146,20 @@ private
     params.require(:project).permit(:title, :description, :start_date, :end_date, :goal, :kpi, :url_targeted, :category, :user_id, :team_id)
   end
 
-end
 
   def set_project
     @project = Project.find(params[:id])
     authorize(@project)
   end
+
+  def set_dates_for_graph(data)
+    @array_date = data.map do |array|
+      array[0]
+    end
+    @array_date_updated = []
+    @array_date.each do |date|
+      @array_date_updated << DateTime.parse(date).to_date.strftime("%0e-%m-%y")
+    end
+    return @array_date_updated
+  end
+end
