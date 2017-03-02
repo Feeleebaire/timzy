58eef322eb2dca4ef129e4626295da886d2d371c
Philibert Latournerie <philibert.latournerie@hotmail.fr>
2017-03-02 13:56:02 +0100

    search okayyyy

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -15,7 +15,7 @@ class TeamsController < ApplicationController
     @team_projects = @team.projects
     # display show according to research (or not)
     @projects = @team_projects.all
-    @projects = @projects.where('title ILIKE ? OR description ILIKE ?', params[:search], params[:search]) if !params[:search].blank?
+    @projects = @projects.where('title ILIKE ? OR description ILIKE ?', "%#{params[:search]}%", "%#{params[:search]}%") if !params[:search].blank?
     @projects = @projects.where(start_date: params[:startdate]..params[:enddate]) if !params[:startdate].blank? && !params[:enddate].blank?
 
     authorize(@team)
