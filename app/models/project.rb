13532eae8fa121b4886412530d256364b20aff38
Sarah Reneaume <sarah.reneaume@gmail.com>
2017-03-02 11:16:58 +0100

    activity-feed completed style refinement needed

--- app/models/project.rb
+++ app/models/project.rb
@@ -12,6 +12,6 @@ class Project < ApplicationRecord
   belongs_to :team
   has_many :comments, dependent: :destroy
   has_many :likes
-
+  has_many :activities, as: :trackable, class_name: 'PublicActivity::Activity', dependent: :destroy
 
 end
