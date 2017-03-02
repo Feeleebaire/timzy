4df10b0b79db4cd88e9bcc9df2351b45443ddf4f
Sarah Reneaume <sarah.reneaume@gmail.com>
2017-03-02 18:10:36 +0100

    done

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,5 +6,6 @@ class Comment < ApplicationRecord
   #associations
   belongs_to :project
   belongs_to :user
+  has_many :activities, as: :trackable, class_name: 'PublicActivity::Activity', dependent: :destroy
 
 end
